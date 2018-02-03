754884fa4481f6a743d7956da2fe07fd7ea477e3
Sa'ar <geger40.1@gmail.com>
2018-02-02 19:22:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 var title = "Code4BelTib";
 $(document).ready(function() {
 	$(".title").text(title);
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+		|| location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		   if (target.length) {
+		     $('html,body').animate({
+			 scrollTop: target.offset().top
+		    }, 1000);
+		    return false;
+		}
+	    }
+	});
 });
