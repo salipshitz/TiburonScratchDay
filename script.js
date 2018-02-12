b96b2650f591cd295832d6529caadbe335314789
Sa'ar <geger40.1@gmail.com>
2018-02-11 21:30:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	console.log("Hello, world!")
 	if (screen.width >= 480) {
 		console.log("gt 480: "+screen.width);
-		$("navbar").addClass("fixed-top");
+		$(".navbar").addClass("fixed-top");
 	} else {
 		console.log("lt 480: "+screen.width);
 	}
