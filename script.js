78a91827a639f62857213289d380762f968e5da5
Sa'ar <geger40.1@gmail.com>
2018-02-11 21:33:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var title = "Code4BelTib";
 $(document).ready(function() {
 	$(".title").append(title);
 	console.log("Hello, world!")
-	if (screen.width >= 1000) {
-		console.log("gt 480: "+screen.width);
+	if (screen.width >= 1500) {
+		console.log("gt 1500: "+screen.width);
 		$(".navbar").addClass("fixed-top");
 	} else {
 		console.log("lt 480: "+screen.width);
