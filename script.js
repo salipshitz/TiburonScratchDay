22e1b63571ba79d91a2feef57b7d693e0955b972
Sa'ar <geger40.1@gmail.com>
2018-04-26 17:15:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var title = "Tiburon Scratch Day";
 $(document).ready(function() {
 	$(".title").text(title);
-	/*while (true) {
-		if (window.innerWidth >= 580) {
+	if (window.innerWidth >= 580) {
 			$(".navbar").addClass("fixed-top");
 		}
-	}*/
+	}
 });
